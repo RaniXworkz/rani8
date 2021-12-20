dbdda56343683d3a287fefcaf8969e0c1cde51e7
RaniXorkz <raniattigeri.xworkz@gmail.com>
2021-12-20 23:45:11 +0530

    hello rajeshwari

---
+++ King4.java
@@ -0,0 +1,13 @@
+class King
+{
+public static void main(String[] b)
+{
+float []ids={2.3,3.4,5.6,7.6,8.9};
+System.out.println(ids[0]);
+System.out.println(ids[1]);
+System.out.println(ids[2]);
+System.out.println(ids[3]);
+System.out.println(ids[4]);
+}
+}
+
